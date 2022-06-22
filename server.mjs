7f2517a0ec7fc2173c98a2d7666ef4b83b89bd9b
Pire123 <55959088+Pire123@users.noreply.github.com>
2022-06-22 23:40:04 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,12 +1,4 @@
 
-//import { createServer } from 'http';
-
-//createServer((req, res) => {
-  //res.writeHead(200)
-  //res.write('haha xd');
-  //res.end();
-//}).listen(process.env.PORT)
-
 const https = require("https");
 const http = require("http");
 
