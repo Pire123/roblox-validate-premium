c4f3ccdc326d9e4ad2e9193e1afb0e96e863f55a
Pire123 <55959088+Pire123@users.noreply.github.com>
2022-06-22 23:28:44 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,21 @@
-import { createServer } from 'http';
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+//import { createServer } from 'http';
+
+//createServer((req, res) => {
+ ////// res.write('Hello World!');
+//  res.end();
+//}).listen(process.env.PORT);
+
+
+http = require('http')
+
+server = http.createServer()
+
+server.on("request",function(req,res){
+  res.writeHead(200)
+  res.write("haha xd")
+  res.end()
+})
+
+server.listen(process.env.PORT)
+
